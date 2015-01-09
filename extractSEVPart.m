707425ce1107afdc4ff678c359f1bd3787d4ed21
Matt Gaidica <matt@gaidi.ca>
2015-01-08 22:44:45 -0500

    single function for making EEG plot

--- extractSEVPart.m
+++ extractSEVPart.m
@@ -1,4 +1,4 @@
-function [sev,header]=extractSEVPart(fileLoc,dataStart,dataLength)
+function [sev,header]=extractSEVPart(fileLoc,dataStart,chunkLength)
     [sev,header] = read_tdt_sev(fileLoc);
-    sev = sev(dataStart:dataStart+dataLength-1);
+    sev = sev(dataStart:dataStart+chunkLength-1);
 end
